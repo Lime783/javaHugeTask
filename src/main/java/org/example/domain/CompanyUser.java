64b7c91f7ed88f804b8f11cc0670b57b37611ca5
Lime783 <hubert.nowocim@gmail.com>
2026-06-24 15:43:31 +0200

    Add 2 types of users: individual and company one

---
+++ src/main/java/org/example/domain/CompanyUser.java
@@ -0,0 +1,41 @@
+package org.example.domain;
+
+import java.util.Objects;
+
+public class CompanyUser extends User {
+
+    private String companyName;
+    private String taxId;
+
+    public CompanyUser(String email, String displayName, String companyName, String taxId) {
+        super(email, displayName);
+        if (companyName.length() > 100 || companyName.length() < 3) {
+            throw new IllegalArgumentException("Invalid company name: " +  companyName);
+        }
+        if (taxId.length() != 5) {
+            throw new IllegalArgumentException("Invalid tax id: " +  taxId);
+        }
+        this.companyName = Objects.requireNonNull(companyName);
+        this.taxId = Objects.requireNonNull(taxId);
+    }
+
+    public CompanyUser(String email, String companyName, String taxId) {
+        this(email, "Anon", companyName, taxId);
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getTaxId() {
+        return taxId;
+    }
+
+    public void setTaxId(String taxId) {
+        this.taxId = taxId;
+    }
+}
